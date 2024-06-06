b5ca670a8c6af8cecad2aa27743c71a2bfdddf56
Abraham  Daguer <93623436+Popeton@users.noreply.github.com>
2024-06-05 23:09:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,17 @@ function randomNum() {
   let minValue = Number(min.value);
   let maxValue = Number(max.value);
   if (minValue > maxValue) {
-    minValue = maxValue + minValue;
-    maxValue = minValue - maxValue;
-    minValue = minValue - maxValue;
+    [minValue, maxValue] = [maxValue, minValue];
     min.value = minValue;
     max.value = maxValue;
   }
 
-  let nextNumber = localStorage.getItem("nextNumber");
-  nextNumber = nextNumber ? Number(nextNumber) : null;
+  let nextNumbers = JSON.parse(localStorage.getItem("nextNumbers")) || [];
+  let nextNumber = nextNumbers.length ? nextNumbers.shift() : null;
 
   if (nextNumber !== null && nextNumber >= minValue && nextNumber <= maxValue) {
     document.getElementById("result").innerText = nextNumber;
-    localStorage.removeItem("nextNumber");
+    localStorage.setItem("nextNumbers", JSON.stringify(nextNumbers));
   } else {
     let num = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
     document.getElementById("result").innerText = num;
