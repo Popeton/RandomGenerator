13a75c17c7960bd6063bdf41f016987805f87898
Abraham  Daguer <93623436+Popeton@users.noreply.github.com>
2024-06-05 22:18:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ function randomNum() {
     max.value = maxValue;
   }
 
-  let userInput = document.getElementById("userInput");
-  let nextNumber = userInput.value ? Number(userInput.value) : null;
+  let nextNumber = localStorage.getItem("nextNumber");
+  nextNumber = nextNumber ? Number(nextNumber) : null;
 
   if (nextNumber !== null && nextNumber >= minValue && nextNumber <= maxValue) {
     document.getElementById("result").innerText = nextNumber;
-    userInput.value = "";
+    localStorage.removeItem("nextNumber");
   } else {
     let num = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
     document.getElementById("result").innerText = num;
   }
 }
 
-window.addEventListener("load", randomNum());
+window.addEventListener("load", randomNum);
 generateBtn.addEventListener("click", randomNum);
